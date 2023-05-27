9abba787a484eab95850ce8a76afc82ef68db035
Dominique Martinet <asmadeus@codewreck.org>
2023-05-28 05:40:26 +0900

    ircd: start to handle connections

--- src/ircd/mod.rs
+++ src/ircd/mod.rs
@@ -1,36 +1,65 @@
-use anyhow::{Context, Error};
-use irc::proto::IrcCodec;
-use log::info;
+use anyhow::{Context, Error, Result};
+use irc::{client::prelude::Command, proto::IrcCodec};
+use log::{debug, info};
+use std::net::SocketAddr;
 use tokio::net::{TcpListener, TcpStream};
 use tokio_util::codec::Framed;
+// for Framed.send()
+//use futures::{SinkExt, StreamExt};
+// for Framed.next()
+use tokio_stream::StreamExt;
 
 use crate::args::args;
 
-async fn handle_client(_stream: Framed<TcpStream, IrcCodec>) {
-    println!("pretend we handled stream");
-}
-
-async fn handle_connection(socket: TcpStream) -> Result<(), Error> {
-    let codec = IrcCodec::new("utf-8")?;
-    let stream = Framed::new(socket, codec);
-    tokio::spawn(async move { handle_client(stream).await });
-    Ok(())
-}
-
 pub async fn listen() -> tokio::task::JoinHandle<()> {
-    let args = args();
-
-    info!("listening to {}", args.ircd_listen);
-    let listener = TcpListener::bind(args.ircd_listen)
+    info!("listening to {}", args().ircd_listen);
+    let listener = TcpListener::bind(args().ircd_listen)
         .await
         .context("bind ircd port")
         .unwrap();
     tokio::spawn(async move {
         while let Ok((socket, addr)) = listener.accept().await {
             info!("Accepted connection from {}", addr);
-            if let Err(e) = handle_connection(socket).await {
-                info!("Could not spawn worker for {}: {}", addr, e);
+            if let Err(e) = handle_connection(socket, addr).await {
+                info!("Could not spawn worker: {}", e);
             }
         }
     })
+}
+
+async fn handle_connection(socket: TcpStream, addr: SocketAddr) -> Result<()> {
+    let codec = IrcCodec::new("utf-8")?;
+    let stream = Framed::new(socket, codec);
+    tokio::spawn(async move {
+        if let Err(e) = handle_client(stream).await {
+            info!("Terminating {}: {}", addr, e)
+        }
+    });
+    Ok(())
+}
+
+async fn handle_client(stream: Framed<TcpStream, IrcCodec>) -> Result<()> {
+    debug!("Awaiting auth");
+    let irc_nick = irc_auth_loop(stream).await?;
+    info!("Login from {}", irc_nick);
+    Ok(())
+}
+
+async fn irc_auth_loop(mut stream: Framed<TcpStream, IrcCodec>) -> Result<String> {
+    let mut client_nick = None;
+    while let Some(event) = stream.try_next().await? {
+        match event.command {
+            Command::NICK(nick) => client_nick = Some(nick),
+            _ => {
+                return Err(Error::msg(format!(
+                    "Unexpected preauth message {:?}",
+                    event
+                )))
+            }
+        }
+    }
+    return match client_nick {
+        Some(nick) => Ok(nick),
+        None => Err(Error::msg("No nick for client!")),
+    };
 }
