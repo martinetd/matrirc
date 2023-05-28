1d9eec54c7ebfbf1d6e2682109945903bcb42020
Dominique Martinet <asmadeus@codewreck.org>
2023-05-29 08:45:42 +0900

    state: back to argon2 + self-rolled chacha20poly1305...
    pwbox was great but adding matrix sdk made a conflict with the hmac version:
    error: failed to select a version for `hmac`.
        ... required by package `matrix-sdk-crypto v0.6.0`
        ... which satisfies dependency `matrix-sdk-crypto = "^0.6.0"` of package `matrix-sdk-sled v0.2.0`
        ... which satisfies dependency `matrix-sdk-sled = "^0.2.0"` of package `matrix-sdk v0.6.0`
        ... which satisfies dependency `matrix-sdk = "^0.6"` of package `matrirc v0.1.0 (/home/shared/matrirc)`
    versions that meet the requirements `^0.12.1` are: 0.12.1
    
    all possible versions conflict with previously selected packages.
    
      previously selected package `hmac v0.12.0`
        ... which satisfies dependency `hmac = "^0.12"` of package `scrypt v0.8.1`
        ... which satisfies dependency `scrypt = "^0.8"` of package `pwbox v0.5.0`
        ... which satisfies dependency `pwbox = "^0.5"` of package `matrirc v0.1.0 (/home/shared/matrirc)`
    
    failed to select a version for `hmac` which could resolve this conflict
    
    So get rid of pwbox and go back to argon2, but implement some
    encryption manually...
    The Aead there does check password just the same.

--- src/state.rs
+++ src/state.rs
@@ -1,36 +1,64 @@
 use anyhow::{Context, Error, Result};
+use argon2::{
+    password_hash::rand_core::{OsRng, RngCore},
+    Argon2,
+};
+use base64_serde::base64_serde_type;
+use chacha20poly1305::{
+    aead::{Aead, NewAead},
+    XChaCha20Poly1305,
+};
 use log::info;
-use pwbox::{sodium::Sodium, ErasedPwBox, Eraser, Suite};
-use rand_core::OsRng;
 use std::fs;
 use std::io::Write;
 use std::os::unix::fs::{DirBuilderExt, OpenOptionsExt};
 use std::path::{Path, PathBuf};
 
+base64_serde_type!(Base64, base64::engine::general_purpose::STANDARD);
+
 use crate::args::args;
 
-// data we want to keep around
+/// data we want to keep around
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct Session {
     pub homeserver: String,
     //pub matrix_session: matrix_sdk::Session;
 }
 
+/// data required for decryption
+#[derive(serde::Serialize, serde::Deserialize)]
+struct Blob {
+    version: String,
+    #[serde(with = "Base64")]
+    ciphertext: Vec<u8>,
+    #[serde(with = "Base64")]
+    salt: Vec<u8>,
+    #[serde(with = "Base64")]
+    nonce: Vec<u8>,
+}
+
 /// try to decrypt session and return it
 fn check_pass(session_file: PathBuf, pass: &str) -> Result<Session> {
     let blob = {
         let content = fs::read(session_file).context("Could not read user session file")?;
-        serde_json::from_slice::<ErasedPwBox>(&content)
+        serde_json::from_slice::<Blob>(&content)
             .context("Could not deserialize session file content")?
     };
-    let mut eraser = Eraser::new();
-    eraser.add_suite::<Sodium>();
-    let unboxed = eraser
-        .restore(&blob)
-        .context("Could not parse data on disk")?
-        .open(pass.as_bytes())
-        .context("Could not decrypt stored session")?;
-    let session = serde_json::from_slice::<Session>(&*unboxed)
+    if blob.version != "argon2+chacha20poly1305" {
+        return Err(Error::msg(
+            "This version only supports argon2+chacha20poly1305",
+        ));
+    }
+    let mut key = [0u8; 32];
+    Argon2::default()
+        .hash_password_into(pass.as_bytes(), &blob.salt, &mut key)
+        .context("Could not hash password")?;
+    let cipher = XChaCha20Poly1305::new(&key.into());
+    let plaintext = cipher
+        .decrypt(blob.nonce.as_slice().into(), &*blob.ciphertext)
+        .context("Could not decrypt blob: bad password?")?;
+
+    let session = serde_json::from_slice::<Session>(&plaintext)
         .context("Could not deserialize stored session")?;
     info!("Decrypted {}", session.homeserver);
     Ok(session)
@@ -38,13 +66,28 @@ fn check_pass(session_file: PathBuf, pass: &str) -> Result<Session> {
 
 /// encrypt session and store it
 pub fn create_user(nick: &str, pass: &str, session: Session) -> Result<()> {
-    let pwbox = Sodium::build_box(&mut OsRng).seal(
-        pass,
-        serde_json::to_vec(&session).context("could not serialize session")?,
-    )?;
-    let mut eraser = Eraser::new();
-    eraser.add_suite::<Sodium>();
-    let blob = eraser.erase(&pwbox)?;
+    let mut key = [0u8; 32];
+    let mut salt = vec![0u8; 32];
+    let mut nonce = vec![0u8; 24];
+    OsRng.fill_bytes(&mut salt);
+    OsRng.fill_bytes(&mut nonce);
+    Argon2::default()
+        .hash_password_into(pass.as_bytes(), &salt, &mut key)
+        .context("Could not hash password")?;
+
+    let cipher = XChaCha20Poly1305::new(&key.into());
+    let ciphertext = cipher
+        .encrypt(
+            nonce.as_slice().into(),
+            &*serde_json::to_vec(&session).context("could not serialize session")?,
+        )
+        .context("Could not encrypt blob")?;
+    let blob = Blob {
+        version: "argon2+chacha20poly1305".to_string(),
+        ciphertext,
+        salt,
+        nonce,
+    };
 
     let user_dir = Path::new(&args().state_dir).join(nick);
     if !user_dir.is_dir() {
