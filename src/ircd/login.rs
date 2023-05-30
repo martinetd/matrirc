d3d3008676b6b8f8727f3b84057bb8ac3dc5607e
Dominique Martinet <asmadeus@codewreck.org>
2023-05-31 05:37:34 +0900

    cleanup: replace erroring 'if let' with 'let else'
    TIL new syntax...
    It's better as in code is less nested if we bail out early on error,
    so use it.

--- src/ircd/login.rs
+++ src/ircd/login.rs
@@ -35,19 +35,19 @@ pub async fn auth_loop(
             _ => (), // ignore
         }
     }
-    if let (Some(nick), Some(user), Some(pass)) = (client_nick, client_user, client_pass) {
-        // need this to be able to interact with irssi: send welcome before any
-        // privmsg exchange even if login isn't over.
-        proto::send_raw_msg(stream, format!(":matrirc 001 {} :Welcome to matrirc", nick)).await?;
-        info!("Processing login from {}!{}", nick, user);
-        let client = match state::login(&nick, &pass)? {
-            Some(session) => matrix_restore_session(stream, &nick, &pass, session).await?,
-            None => matrix_login_loop(stream, &nick, &pass).await?,
-        };
-        Ok((nick, user, client))
-    } else {
-        Err(Error::msg("nick or pass wasn't set for client!"))
-    }
+
+    let (Some(nick), Some(user), Some(pass)) = (client_nick, client_user, client_pass) else {
+        return Err(Error::msg("nick or pass wasn't set for client!"))
+    };
+    // need this to be able to interact with irssi: send welcome before any
+    // privmsg exchange even if login isn't over.
+    proto::send_raw_msg(stream, format!(":matrirc 001 {} :Welcome to matrirc", nick)).await?;
+    info!("Processing login from {}!{}", nick, user);
+    let client = match state::login(&nick, &pass)? {
+        Some(session) => matrix_restore_session(stream, &nick, &pass, session).await?,
+        None => matrix_login_loop(stream, &nick, &pass).await?,
+    };
+    Ok((nick, user, client))
 }
 
 async fn matrix_login_loop(
@@ -66,38 +66,46 @@ async fn matrix_login_loop(
         trace!("matrix connection loop: got {:?}", event);
         match event.command {
             Command::PRIVMSG(_, body) => {
-                if let [homeserver, user, pass] = body.splitn(3, ' ').collect::<Vec<&str>>()[..] {
+                let [homeserver, user, pass] = body.splitn(3, ' ').collect::<Vec<&str>>()[..] else {
                     proto::send_privmsg(
                         stream,
                         "matrirc",
                         nick,
-                        format!("Attempting to login to {} with {}", homeserver, user),
+                        format!("Message not in <homeserver> <user> <pass> format, ignoring."),
                     )
                     .await?;
-                    match matrix::login::login(homeserver, user, pass, nick, irc_pass).await {
-                        Ok(client) => {
-                            state::create_user(
-                                &nick,
-                                &irc_pass,
-                                state::Session {
-                                    homeserver: homeserver.into(),
-                                    matrix_session: client
-                                        .session()
-                                        .context("client has no session")?,
-                                },
-                            )?;
-                            return Ok(client);
-                        }
-                        Err(e) => {
-                            proto::send_privmsg(
-                                stream,
-                                "matrirc",
-                                nick,
-                                format!("Login failed: {}. Try again.", e),
-                            )
-                            .await?;
-                            continue;
-                        }
+                    continue;
+                };
+                proto::send_privmsg(
+                    stream,
+                    "matrirc",
+                    nick,
+                    format!("Attempting to login to {} with {}", homeserver, user),
+                )
+                .await?;
+                match matrix::login::login(homeserver, user, pass, nick, irc_pass).await {
+                    Ok(client) => {
+                        state::create_user(
+                            &nick,
+                            &irc_pass,
+                            state::Session {
+                                homeserver: homeserver.into(),
+                                matrix_session: client
+                                    .session()
+                                    .context("client has no session")?,
+                            },
+                        )?;
+                        return Ok(client);
+                    }
+                    Err(e) => {
+                        proto::send_privmsg(
+                            stream,
+                            "matrirc",
+                            nick,
+                            format!("Login failed: {}. Try again.", e),
+                        )
+                        .await?;
+                        continue;
                     }
                 }
             }
