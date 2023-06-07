3a1a895d4758cc5a99e2f9f5ae7a045b85ea1211
Dominique Martinet <asmadeus@codewreck.org>
2023-06-08 06:38:49 +0900

    room mappings: fix room names and some more
    - use room members directly instead of using active_members_count
    as that doesn't seem to work like we want it to
    - rework room targets a bit
    - prepare to fill in room members

--- src/matrix/room_mappings.rs
+++ src/matrix/room_mappings.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Error, Result};
 use irc::client::prelude::{Command, Message};
 use lazy_static::lazy_static;
 use log::info;
@@ -9,6 +9,7 @@ use matrix_sdk::{
     RoomMemberships,
 };
 use regex::Regex;
+use std::borrow::Cow;
 use std::collections::hash_map::{Entry, HashMap};
 use std::sync::Arc;
 use tokio::sync::{Mutex, RwLock, RwLockReadGuard};
@@ -21,15 +22,17 @@ use crate::matrirc::Matrirc;
 
 #[derive(Debug, Clone)]
 struct Chan {
-    /// channel name
-    chan: String,
+    /// channel name or query target
+    target: String,
     /// matrix user -> nick for channel.
     /// display names is a per-channel property, so we need to
-    /// remember this for each user individually
+    /// remember this for each user individually.
+    /// In queries case, any non-trivial member is expanded as <nick> at
+    /// the start of the message
     members: HashMap<OwnedUserId, String>,
     /// list of irc names in channel
     /// used to enforce unicity, and perhaps later to convert
-    /// mentions: to matric mentions
+    /// `mentions:` to matric mentions
     names: HashMap<String, OwnedUserId>,
 }
 
@@ -48,24 +51,16 @@ struct JoiningChan {
 }
 
 #[derive(Debug, Clone)]
-struct Query {
-    /// query name
-    target: String,
-    /// used to differentiate ourselves from query target
-    /// when we speak on another client
-    own_id: OwnedUserId,
-}
-
-#[derive(Debug, Clone)]
 pub struct RoomTarget {
     /// the Arc/RwLock let us return/modify it without holding the mappings lock
     inner: Arc<RwLock<RoomTargetInner>>,
+    error: Arc<Option<String>>,
 }
 
 #[derive(Debug, Clone)]
 enum RoomTargetInner {
     /// room maps to a query e.g. single other member (or alone!)
-    Query(Query),
+    Query(Chan),
     /// room maps to a chan, and irc side has it joined
     Chan(Chan),
     /// room maps to a chan, but we're not joined: will force join
@@ -85,13 +80,13 @@ pub struct Mappings {
 struct MappingsInner {
     /// matrix room id to either chan or query
     rooms: HashMap<OwnedRoomId, RoomTarget>,
-    /// chan name to room id, names are registered and reserved even if not joined,
+    /// chan/query name to room id,
+    /// channel names are registered and reserved even if not joined,
     /// but there can be rooms we haven't seen yet
     /// TODO: add a metacommand to force iterating Matrirc.matrix().rooms() ?
     /// (probably want this to list available query targets too...)
-    chans: HashMap<String, Arc<OwnedRoomId>>,
-    /// query name to room id; note 'matrirc' and own irc nick are reserved.
-    queries: HashMap<String, Arc<OwnedRoomId>>,
+    /// TODO: also reserve 'matrirc', irc.nick()...
+    targets: HashMap<String, Arc<OwnedRoomId>>,
 }
 
 fn sanitize<'a, S: Into<String>>(str: S) -> String {
@@ -103,9 +98,9 @@ fn sanitize<'a, S: Into<String>>(str: S) -> String {
 }
 
 impl Chan {
-    fn new(chan: String) -> Self {
+    fn new(target: String) -> Self {
         Chan {
-            chan,
+            target,
             members: HashMap::new(),
             names: HashMap::new(),
         }
@@ -116,28 +111,31 @@ impl Chan {
 }
 
 impl JoiningChan {
-    fn new(chan: String) -> Self {
+    fn new(target: String) -> Self {
         JoiningChan {
-            chan: Chan::new(chan),
+            chan: Chan::new(target),
             pending_messages: Vec::new(),
         }
     }
 }
 
 impl RoomTarget {
-    fn query<'a, S: Into<String>>(target: S, own_id: OwnedUserId) -> Self {
+    fn query<'a, S: Into<String>>(target: S) -> Self {
         RoomTarget {
-            inner: Arc::new(RwLock::new(RoomTargetInner::Query(Query {
+            inner: Arc::new(RwLock::new(RoomTargetInner::Query(Chan {
                 target: sanitize(target),
-                own_id,
+                members: HashMap::new(),
+                names: HashMap::new(),
             }))),
+            error: Arc::new(None),
         }
     }
-    fn new_chan<'a, S: Into<String>>(chan_name: S) -> Self {
+    fn chan<'a, S: Into<String>>(chan_name: S) -> Self {
         RoomTarget {
             inner: Arc::new(RwLock::new(RoomTargetInner::JoiningChan(JoiningChan::new(
                 sanitize(chan_name),
             )))),
+            error: Arc::new(None),
         }
     }
 
@@ -156,54 +154,20 @@ impl RoomTarget {
         Ok(messages)
     }
 
-    async fn query_of_room(room: &Room) -> Self {
-        // XXX: missing dedup, we need to pass mappings in argument
-        // so we can check queries and add _x or whatever
-        // Also pre-fill 'matrirc' and irc.nick in that map (or
-        // add another map for special queries...)
-        match room.members(RoomMemberships::ACTIVE).await {
-            Ok(members) => {
-                let other_member = if room.active_members_count() == 2
-                    && members[0].user_id() == room.own_user_id()
-                {
-                    &members[1]
-                } else {
-                    &members[0]
-                };
-                // XXX perhaps don't set our own user id if we're alone,
-                // to avoid the from self prefix all the time for a note room?
-                return RoomTarget::query(
-                    other_member.name().to_string(),
-                    room.own_user_id().to_owned(),
-                );
-            }
-            Err(error) => {
-                info!("Error getting room members: {}", error);
-                // XXX fail harder here? won't be able to track who said what...
-            }
-        };
-        let name = match room.display_name().await {
-            Ok(room_name) => room_name.to_string(),
-            Err(error) => {
-                info!("Error getting room display name: {}", error);
-                room.room_id().to_string()
-            }
-        };
-        // feed a fake user id to query so we always prefix messages,
-        // as it's not normal we didn't find out who it was.
-        RoomTarget::query(name, room.own_user_id().to_owned())
+    fn set_error(mut self, error: String) -> Self {
+        self.error = Arc::new(Some(error));
+        self
     }
 
-    async fn join_chan_of_room(room: &Room) -> Self {
-        let room_name = match room.display_name().await {
-            Ok(room_name) => room_name.to_string(),
-            Err(error) => {
-                info!("Error getting room display name: {}", error);
-                // Fallback to the room ID.
-                room.room_id().to_string()
-            }
-        };
-        RoomTarget::new_chan(room_name)
+    async fn target_of_room(name: String, room: &Room) -> Result<(Self, Vec<RoomMember>)> {
+        // XXX we don't want this to be long: figure out active_members_count
+        // https://github.com/matrix-org/matrix-rust-sdk/issues/2010
+        let members = room.members(RoomMemberships::ACTIVE).await?;
+        match members.len() {
+            0 => Err(Error::msg(format!("Message in empty room {}?", name))),
+            1 | 2 => Ok((RoomTarget::query(name), members)),
+            _ => Ok((RoomTarget::chan(name), members)),
+        }
     }
 
     pub async fn send_irc_message<'a, S>(
@@ -221,8 +185,8 @@ impl RoomTarget {
                 message_type,
                 from: target.target.clone(),
                 target: irc.nick.clone(),
-                message: if sender_id == target.own_id {
-                    format!("<from self> {}", message)
+                message: if let Some(nick) = target.members.get(sender_id) {
+                    format!("<{}> {}", nick, message)
                 } else {
                     message.into()
                 },
@@ -231,23 +195,46 @@ impl RoomTarget {
             // XXX chans are still queries at this point
             RoomTargetInner::Chan(chan) => IrcMessage {
                 message_type,
-                from: chan.chan.clone(),
+                from: chan.target.clone(),
                 target: irc.nick.clone(),
-                message: format!("<from {}> {}", sender_id, message),
+                message: format!(
+                    "<{}> {}",
+                    chan.members
+                        .get(sender_id)
+                        .map(Cow::Borrowed)
+                        .unwrap_or_else(|| Cow::Owned(sender_id.to_string())),
+                    message
+                ),
             },
             // This one should trigger a join and queue message
             RoomTargetInner::LeftChan(chan) => IrcMessage {
                 message_type,
-                from: chan.chan.clone(),
+                from: chan.target.clone(),
                 target: irc.nick.clone(),
-                message: format!("<from {}> {}", sender_id, message),
+                message: format!(
+                    "<{}> {}",
+                    chan.members
+                        .get(sender_id)
+                        .map(Cow::Borrowed)
+                        .unwrap_or_else(|| Cow::Owned(sender_id.to_string())),
+                    message
+                ),
             },
             // This one should just queue message
             RoomTargetInner::JoiningChan(jchan) => IrcMessage {
                 message_type,
-                from: jchan.chan.chan.clone(),
+                from: jchan.chan.target.clone(),
                 target: irc.nick.clone(),
-                message: format!("<from {}> {}", sender_id, message),
+                message: format!(
+                    "<{}> {}",
+                    jchan
+                        .chan
+                        .members
+                        .get(sender_id)
+                        .map(Cow::Borrowed)
+                        .unwrap_or_else(|| Cow::Owned(sender_id.to_string())),
+                    message
+                ),
             },
         }
         .into();
@@ -257,44 +244,60 @@ impl RoomTarget {
 
 impl Mappings {
     pub async fn room_target(&self, room: &Room) -> RoomTarget {
+        match self.try_room_target(room).await {
+            Ok(target) => target,
+            Err(e) => {
+                // return temporary error channel
+                RoomTarget::query("matrirc")
+                    .set_error(format!("Could not find or create target: {}", e))
+            }
+        }
+    }
+    async fn try_room_target(&self, room: &Room) -> Result<RoomTarget> {
         // happy case first
         if let Some(target) = self.inner.read().await.rooms.get(room.room_id()) {
-            return target.clone();
+            return Ok(target.clone());
         }
-        let target = if room.active_members_count() <= 2 {
-            RoomTarget::query_of_room(room).await
-        } else {
-            RoomTarget::join_chan_of_room(room).await
-        };
-        // let's try to insert it...
-        match self.inner.write().await.rooms.entry(room.room_id().into()) {
-            Entry::Occupied(entry) => {
-                // got raced, just return whatever we found and throw away our hard work
-                return entry.get().clone();
+        // create anew and try to insert it...
+        // XXX: missing dedup, check queries and add _x or whatever
+        // Also forbid 'matrirc' and irc.nick in that map (or
+        // add another map for special queries...)
+        // XXX: even if done here we need to do this under lock:
+        // 1/ get mappings write lock, 2/ check race, 3/ find free name,
+        // 4/ create target and 5/ insert to both forward and revert mappings
+        // 6/ get room write lock, 7/ release mappings lock
+        // 8/ fill in room members, 9/ start join process if room?
+        //
+        let name = match room.display_name().await {
+            Ok(room_name) => room_name.to_string(),
+            Err(error) => {
+                info!("Error getting room display name: {}", error);
+                room.room_id().to_string()
             }
-            Entry::Vacant(entry) => entry.insert(target.clone()),
         };
-        target
-        // XXX get room.active_member_count() to decide chan or query
-        // member list with room.members(RoomMemberships::JOIN) only on join
-        // if chan: ↓ + XXX get room topic
-    }
-    async fn join_chan(&self, room: &Room) -> Result<Vec<Message>> {
-        let lock = self.inner.read().await;
-        let Some(target) = lock.rooms.get(room.room_id()) else {
-            return Err(anyhow::Error::msg(format!("Trying to join a room we didn't find: {}", room.room_id())))
+        let target = {
+            let mut mappings = self.inner.write().await;
+            if let Some(target) = mappings.rooms.get(room.room_id()) {
+                // got raced
+                return Ok(target.clone());
+            }
+            // XXX check dups here
+            let (target, _members) = RoomTarget::target_of_room(name.clone(), room).await?;
+            mappings.rooms.insert(room.room_id().into(), target.clone());
+            mappings
+                .targets
+                .insert(name, room.room_id().to_owned().into());
+            // XXX lock target here
+            target
         };
-        target.join_chan().await
+        // XXX fill in target members here
+        // XXX: start task to start join process (needs irc...)
+        Ok(target)
     }
     // XXX promote/demote chans on join/leave events:
     // 1 -> 2 active, check for name/rename query
     // 2 -> 3+, convert from query to chan
     // 3+ -> 3, demote to query?
     // 2 -> 1, rename to avoid confusion?
-    async fn user_nick(&self, user_id: &UserId, member: Option<RoomMember>) -> Option<String> {
-        // lookup in users list
-        // if not fund get display name, ensure unicity through nick_list... or not? display names
-        // can be different per room? check this further..
-        None
-    }
+    // XXX update room mappings on join/leave events...
 }
