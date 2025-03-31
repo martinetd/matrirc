9cf2761ab16a32ecb02fd53168187f16c79623b7
Simon Schuster <git@rationality.eu>
2025-03-31 19:30:48 +0200

    Add --autojoin option to select whether to autojoin channels and queries

--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,25 @@
 use clap::Parser;
 use lazy_static::lazy_static;
+use serde::Deserialize;
 use std::net::SocketAddr;
+
+#[derive(clap::ValueEnum, Clone, Debug, PartialEq, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum AutoJoinOptions {
+    None,
+    Queries,
+    Channels,
+    All,
+}
+
+impl AutoJoinOptions {
+    pub fn join_queries(&self) -> bool {
+        [AutoJoinOptions::Queries, AutoJoinOptions::All].contains(self)
+    }
+    pub fn join_channels(&self) -> bool {
+        [AutoJoinOptions::Channels, AutoJoinOptions::All].contains(self)
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -19,6 +38,9 @@ pub struct Args {
 
     #[arg(long, default_value = None)]
     pub media_url: Option<String>,
+
+    #[arg(long, value_enum, default_value_t = AutoJoinOptions::None)]
+    pub autojoin: AutoJoinOptions,
 }
 
 pub fn args() -> &'static Args {
